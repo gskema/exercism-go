e214c910f8b0c9822ed6e0235cba70920a4ce9d9
Gytis Skema <gytis.skema@gmail.com>
2016-12-29 21:33:23 +0200

    Leap problem solution

--- leap/leap.go
+++ leap/leap.go
@@ -1,12 +1,12 @@
-// Leap stub file
-
-// The package name is expected by the test program.
+/*
+Package leap provides leap year calculation function
+*/
 package leap
 
 // testVersion should match the targetTestVersion in the test file.
 const testVersion = 2
 
-// It's good style to write a comment here documenting IsLeapYear.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
+// Returns true when year is a leap year
+func IsLeapYear(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
 }
