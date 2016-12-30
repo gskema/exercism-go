a1bd005543090b6a09a36f3cecd8d3b9952acca2
Gytis Skema <gytis.skema@gmail.com>
2016-12-30 20:22:37 +0200

    hamming solution

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,7 +1,23 @@
+// Package hamming provides utils to calculate Hamming distance
+// between two strings
 package hamming
+
+import "errors"
 
 const testVersion = 5
 
+// Distance calculates Hamming distance between two strings
 func Distance(a, b string) (int, error) {
-	// your code here!
+	if len(a) != len(b) {
+		return 0, errors.New("String length must be equal")
+	}
+
+	var distance int = 0
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+
+	return distance, nil
 }
